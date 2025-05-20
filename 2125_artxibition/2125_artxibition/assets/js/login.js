23d9675922169a5e1ad987e4dc4910ab370dd08c
Beyza <130453637+beyyzayyavuz@users.noreply.github.com>
2025-05-21 00:28:08 +0300

    Changes

--- 2125_artxibition/2125_artxibition/assets/js/login.js
+++ 2125_artxibition/2125_artxibition/assets/js/login.js
@@ -1,30 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.querySelector(".form-box.login");
   const registerForm = document.querySelector(".form-box.register");
-  const loginLink = document.querySelector(".login-link");
-  const registerLink = document.querySelector(".register-link");
+  const guestForm = document.querySelector(".form-box.guest");
+
+  const loginLinks = document.querySelectorAll(".login-link");
+  const registerLinks = document.querySelectorAll(".register-link");
+  const guestLinks = document.querySelectorAll(".guest-link");
 
   // Başlangıç durumu
   loginForm.classList.add("active");
   registerForm.classList.add("exit-left");
+  guestForm.classList.add("exit-left");
 
-  // Register’a tıklanırsa geçiş yap
-  registerLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    loginForm.classList.remove("active");
-    loginForm.classList.add("exit-left");
+  // Register’a geçiş
+  registerLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      loginForm.classList.remove("active");
+      guestForm.classList.remove("active");
 
-    registerForm.classList.remove("exit-left");
-    registerForm.classList.add("active");
+      loginForm.classList.add("exit-left");
+      guestForm.classList.add("exit-left");
+
+      registerForm.classList.remove("exit-left");
+      registerForm.classList.add("active");
+    });
   });
 
-  // Login’e tıklanırsa geri geçiş yap
-  loginLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    registerForm.classList.remove("active");
-    registerForm.classList.add("exit-left");
+  // Login’e geçiş
+  loginLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      registerForm.classList.remove("active");
+      guestForm.classList.remove("active");
 
-    loginForm.classList.remove("exit-left");
-    loginForm.classList.add("active");
+      registerForm.classList.add("exit-left");
+      guestForm.classList.add("exit-left");
+
+      loginForm.classList.remove("exit-left");
+      loginForm.classList.add("active");
+    });
+  });
+
+  // Guest formuna geçiş
+  guestLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      loginForm.classList.remove("active");
+      registerForm.classList.remove("active");
+
+      loginForm.classList.add("exit-left");
+      registerForm.classList.add("exit-left");
+
+      guestForm.classList.remove("exit-left");
+      guestForm.classList.add("active");
+    });
   });
 });
